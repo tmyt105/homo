cccfb68aebfc246516bfe60564c78f7dc10e5433
timu-119 <tmyt105@gmail.com>
2024-12-21 19:52:14 +0900

    Update homo.js

--- homo.js
+++ homo.js
@@ -1,5 +1,57 @@
-export const NumberTable = {
-  656100: "810*810",
+const homo = ((Nums) => {
+	const numsReversed = Object.keys(Nums).map(x => +x).filter(x => x > 0)
+	const getMinDiv = (num) => {
+		for (let i = numsReversed.length; i >= 0; i--)
+			if (num >= numsReversed[i])
+				return numsReversed[i]
+	}
+	const isDotRegex = /\.(\d+?)0{0,}$/
+	const demolish = (num) => {
+		if (typeof num !== "number")
+			return ""
+
+		if (num === Infinity || Number.isNaN(num))
+			return `这么恶臭的${num}有必要论证吗`
+
+		if (num < 0)
+			return `(⑨)*(${demolish(num * -1)})`.replace(/\*\(1\)/g, "")
+
+		if (!Number.isInteger(num)) {
+			// abs(num) is definitely smaller than 2**51
+			// rescale
+			const n = num.toFixed(16).match(isDotRegex)[1].length
+			return `(${demolish(num * Math.pow(10, n))})/(10)^(${n})`
+		}
+
+		if (Nums[num])
+			return String(num)
+
+		const div = getMinDiv(num)
+		return (`${div}*(${demolish(Math.floor(num / div))})+` +
+			`(${demolish(num % div)})`).replace(/\*\(1\)|\+\(0\)$/g, "")
+	}
+	//Finisher
+	const finisher = (expr) => {
+		expr = expr.replace(/\d+|⑨/g, (n) => Nums[n]).replace("^", "**")
+		//As long as it matches ([\*|\/])\(([^\+\-\(\)]+)\), replace it with $1$2
+		while (expr.match(/[\*|\/]\([^\+\-\(\)]+\)/))
+			expr = expr.replace(/([\*|\/])\(([^\+\-\(\)]+)\)/, (m, $1, $2) => $1 + $2)
+		//As long as it matches ([\+|\-])\(([^\(\)]+)\)([\+|\-|\)]), replace it with $1$2$3
+		while (expr.match(/[\+|\-]\([^\(\)]+\)[\+|\-|\)]/))
+			expr = expr.replace(/([\+|\-])\(([^\(\)]+)\)([\+|\-|\)])/, (m, $1, $2, $3) => $1 + $2 + $3)
+		//As long as it matches ([\+|\-])\(([^\(\)]+)\)$, replace it with $1$2
+		while (expr.match(/[\+|\-]\(([^\(\)]+)\)$/))
+			expr = expr.replace(/([\+|\-])\(([^\(\)]+)\)$/, (m, $1, $2) => $1 + $2)
+		//If there is a bracket in the outermost part, remove it
+		if (expr.match(/^\([^\(\)]+?\)$/))
+			expr = expr.replace(/^\(([^\(\)]+)\)$/, "$1")
+
+		expr = expr.replace(/\+-/g,'-')
+		return expr
+	}
+	return (num) => finisher(demolish(num))
+})({
+	 656100: "810*810",
   65610: "810*(81-0)",
   6561: "(81+0)*(81+0)",
   828: "810+8+10",
@@ -27,133 +79,8 @@ export const NumberTable = {
   2: "8+1+0-8+1-0",
   1: "8+1+0-8*1-0",
   0: "8*1*0",
-  [-1]: "8*1+0-8-1-0"
-}
+  [-1]: "8*1+0-8-1-0",
+})
 
-/**
- * @description homo
- * @param {number} number
- * @returns {DemolishResult}
- */
-const homo = () => {
-  const numsReversed = Object.keys(NumberTable)
-    .map(x => +x)
-    .filter(x => x > 0)
-  const getMinDiv = num => {
-    for (let i = numsReversed.length | 0; i >= 0; i = (i - 1) | 0) {
-      if (num >= numsReversed[i]) {
-        return numsReversed[i]
-      }
-    }
-
-    return null
-  }
-  const isDotRegex = /\.(\d+?)0{0,}$/
-  const demolish = num => {
-    if (num === Infinity || Number.isNaN(num) || typeof num !== "number")
-      return {
-        success: false,
-        result: "Invalid Number"
-      }
-
-    if (num < 0) {
-      const demolishRevResult = demolish(num * -1)
-      if (!demolishRevResult.success) return demolishRevResult
-
-      return {
-        success: true,
-        result: `(-1)*(${demolishRevResult.result})`.replace(/\*\(1\)/g, "")
-      }
-    }
-
-    if (!Number.isInteger(num)) {
-      const match = num.toFixed(16).match(isDotRegex)
-
-      if (!match) {
-        return {
-          success: false,
-          result: "Invalid Number"
-        }
-      }
-
-      const n = match[1].length
-
-      const demolishDecResult = demolish(num * Math.pow(10, n))
-
-      if (!demolishDecResult) return demolishDecResult
-
-      return {
-        success: true,
-        result: `(${demolishDecResult.result})/(10)^(${n})`
-      }
-    }
-
-    if (num in NumberTable) {
-      return {
-        success: true,
-        result: String(num)
-      }
-    }
-
-    const div = getMinDiv(num)
-
-    if (!div) {
-      return {
-        success: false,
-        result: "Impossible number"
-      }
-    }
-
-    const demolishQuoResult = demolish(Math.floor(num / div))
-    if (!demolishQuoResult.success) return demolishQuoResult
-
-    const demolishRemResult = demolish(num % div)
-    if (!demolishRemResult.success) return demolishRemResult
-
-    return {
-      success: true,
-      result: (
-        `${div}*(${demolishQuoResult.result})+` +
-        `(${demolishRemResult.result})`
-      ).replace(/\*\(1\)|\+\(0\)$/g, "")
-    }
-  }
-
-  const finisher = expr => {
-    expr = expr
-      .replace(/\d+|-1/g, n => (n in NumberTable ? NumberTable[Number(n)] : ""))
-      .replace("^", "**")
-    while (expr.match(/[\*|\/]\([^\+\-\(\)]+\)/))
-      expr = expr.replace(
-        /([\*|\/])\(([^\+\-\(\)]+)\)/,
-        (_m, $1, $2) => $1 + $2
-      )
-    while (expr.match(/[\+|\-]\([^\(\)]+\)[\+|\-|\)]/))
-      expr = expr.replace(
-        /([\+|\-])\(([^\(\)]+)\)([\+|\-|\)])/,
-        (_m, $1, $2, $3) => $1 + $2 + $3
-      )
-    while (expr.match(/[\+|\-]\(([^\(\)]+)\)$/))
-      expr = expr.replace(/([\+|\-])\(([^\(\)]+)\)$/, (_m, $1, $2) => $1 + $2)
-    if (expr.match(/^\([^\(\)]+?\)$/))
-      expr = expr.replace(/^\(([^\(\)]+)\)$/, "$1")
-
-    expr = expr.replace(/\+-/g, "-")
-    return expr
-  }
-
-  return num => {
-    const demolishResult = demolish(num)
-
-    if (demolishResult.success) {
-      return {
-        success: true,
-        result: finisher(demolishResult.result)
-      }
-    } else {
-      return demolishResult
-    }
-  }
-}
-
-export { homo }
+if (typeof module === 'object' && module.exports)
+	module.exports = homo
